aa5938737b64ffc98f77c5ba276b4b023788a8c3
Twilight <631218831@qq.com>
2019-04-15 16:11:27 +0800

    init

---
+++ src/main/java/com/zsoft/spring/boot/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.zsoft.spring.boot.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("hello")
+    public String hello(){
+        return "<font style='font-size:28px;'>Hello Spring Boot</font>";
+    }
+}
